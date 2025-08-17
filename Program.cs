3a432ce07431a88091da1450ccfa1b9f3ec89c5a
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-08-17 09:45:26 -0300

    Projeto de desafio de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("489999999", "Iphone 6", "12364", 128);
+Nokia nokia = new Nokia("489999999", "Nokia 2", "4321", 256);
+
+Console.WriteLine(iphone.Numero);
+Console.WriteLine(nokia.Numero);
+
+iphone.Numero = "488999999";
+nokia.Numero = "4878999999";
+
+iphone.InstalarAplicativo("Cand crush");
+nokia.InstalarAplicativo("Fruit ninja");
+
+Console.WriteLine(iphone.Numero);
+Console.WriteLine(nokia.Numero);
